c2f8812a538aae4921ba407c8d967080c319a44a
vamsi0750 <nagisettyvamsi143@gmail.com>
2019-08-15 15:48:00 +0530

    added models

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+
+  res.send("User Route");
+});
+
+module.exports = router;
