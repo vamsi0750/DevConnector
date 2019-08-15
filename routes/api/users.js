569a15b5296b621de972c00d68db2b32250557de
vamsi0750 <nagisettyvamsi143@gmail.com>
2019-08-15 16:42:42 +0530

    sign up

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,59 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+const User = require("../../models/User");
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a passwor d with 6 or more character"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  res.send("User Route");
-});
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User alredy exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const slat = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, slat);
+      await user.save();
+
+      res.send("User Registered");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
