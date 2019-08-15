9942e7a06dc83f29d81281f8e774295d66ffb7a7
vamsi0750 <nagisettyvamsi143@gmail.com>
2019-08-15 14:18:13 +0530

    first commit -1

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`server started on port  ${PORT}`);
 });
